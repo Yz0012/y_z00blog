a788b89639392aed4eb44b1fe02b4d461c1d8d83
Yz0012 <yyzz00@foxmail.com>
2026-09-07 20:42:03 +0800

    Test:workflows

--- subconvert.js
+++ subconvert.js
@@ -53,8 +53,8 @@ const TEMPLATE = `---
     <meta charset="UTF-8">
     <link rel="icon" href="{{ site.baseurl }}/MainImage/profile.jpg">
     <link rel="stylesheet" href="{{ site.baseurl }}/defaulttheme.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/line-numbers/prism-line-numbers.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-dark.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/plugins/line-numbers/prism-line-numbers.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.18.5/dist/katex.min.css" integrity="sha384-2dNi/m6JtSiviznrOIZ5fTiZ5As0In2QwkuXSgoqcQtCNplvJAbt+jveeN+8en73" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title></title>
